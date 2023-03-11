b7dfa867b95533cb25ae688858be4c140d4e699a
Zain5316 <luthfizain45@gmail.com>
2023-03-11 21:41:16 +0700

    update

---
+++ Modul_1/Latihan_Praktikum/Konsumsi.java
@@ -0,0 +1,17 @@
+public class Konsumsi<Element, Element1> {
+    private Element makanan;
+    private Element1 minuman;
+
+    public Element getMakanan(){
+        return makanan;
+    }
+
+    public Element1 getMinuman() {
+        return minuman;
+    }
+
+    public void setKonsumsi(Element makanan, Element1 minuman){
+        this.makanan = makanan;
+        this.minuman = minuman;
+    }
+}
---
+++ Modul_1/Latihan_Praktikum/Kota.java
@@ -0,0 +1,18 @@
+public class Kota<Element> {
+    private Element element;
+
+    public Kota(Element kota){
+        element = kota;
+    }
+
+    public Element getElement() {
+        return element;
+    }
+
+    public static void main(String[] args) {
+        Kota<Integer> jumlahkota = new Kota<Integer>(9);
+        Kota<String> namakota = new Kota<String>("Malang");
+        System.out.println("Jumlah Kota Di Jawa Timur : " + jumlahkota.getElement() + " Kota");
+        System.out.println("Salah satu kota di Jawa Timur : Kota " + namakota.getElement());
+    }
+}
